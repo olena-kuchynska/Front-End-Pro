08c9e3d10a7b38008993448ac51c96c485e20356
Olena-Bulatova <bulatova853@gmail.com>
2020-02-23 12:43:04 +0200

    Add lesson 9

---
+++ lesson_9/task_1/js/script.js
@@ -0,0 +1,43 @@
+/*1.Напишите функцию, которая сравнивает два объекта(ключ и значение одинаковые) и копирует их в новый объект, 
+где свойства будут значениями, а значения ключами.*/
+
+let firstObject = {
+    name: 'Olena',
+    age: 24,
+    dateOfBith: '01.01.1996',
+    today: '23.02.20',
+    gender: 'woman'    
+}
+
+let secondObject = {
+    name: 'Olena',
+    age: 24,
+    dateOfBith: '01.01.1996',
+    today: '22.02.20',  
+}
+console.log(firstObject);
+console.log(secondObject);
+
+function getNewObject(firstObject, secondObject) {
+
+    let newObject = {};
+
+    for(let x in firstObject) {
+
+        for(let y in secondObject) {
+
+            if(x === y && firstObject[x] === secondObject[y]) {
+                
+                newObject[firstObject[x]] = x;
+
+            }
+
+        }
+
+    }
+
+    return newObject;
+
+}
+
+console.log(getNewObject(firstObject,secondObject));
---
+++ lesson_9/task_2/js/script.js
@@ -0,0 +1,29 @@
+/*2. Пользователь вводит строку, разбиваем ее на элементы массива. 
+Создаём объект и наполняем его элементами из масмива: нечётные- ключи, чётные- значения*/
+
+let sentence = prompt('Enter any sentence:');
+let arrOfSentence = sentence.split(' ');
+let objectOfSentence = {};
+
+for(let i = 0; i < arrOfSentence.length; i++) {
+    
+    if(i % 2 <= 0) {
+
+        objectOfSentence[arrOfSentence[i]] = arrOfSentence[i + 1];
+        i++;
+
+    }
+
+}
+
+/*arrOfSentence.forEach((item,index,arr) => {
+    
+    if(index % 2 <= 0) {
+        objectOfSentence[item] = arr[index + 1];
+        index++;
+    }
+
+});*/
+
+console.log(objectOfSentence);
+
