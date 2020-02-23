1d7be9a5e07ed2dcc4b4e2ab5aa8cabbeb980473
Olena-Bulatova <bulatova853@gmail.com>
2020-02-23 12:54:54 +0200

    Edit name of folders

---
+++ lesson_9/js/script.js
@@ -0,0 +1,14 @@
+//10-е задание срез
+
+function getLetter(str) {
+    var reg = /[аоиеёэыуюя]/gi;
+    var count = str.match(reg);
+    console.log(count);
+    if(count===null) {
+    return 'В фразе нет согласных букв';
+    } else {
+    return count.length;
+    }
+    
+    }
+    console.log('Кол-во гласных букв фразе: ' + getLetter('Введите цвет сфетофора'));
